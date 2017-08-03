50f6cfc05663c84e4a7d6cef173b4bf47d30d3b7
Nitin Tulswani <tulswani19@gmail.com>
2017-08-03 22:49:59 +0530

    Delete native.js

--- src/native.js
+++
@@ -1,7 +0,0 @@
-import glamorous, { ThemeProvider, withTheme } from 'glamorous-native'
-import assignAliases from './aliases'
-
-// For React Native
-export default assignAliases(glamorous)
-
-export { ThemeProvider, withTheme }
