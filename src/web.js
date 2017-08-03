ec94b59139679ac1ba2c6f33262788cd5c7a01bf
Nitin Tulswani <tulswani19@gmail.com>
2017-08-03 22:50:08 +0530

    Delete web.js

--- src/web.js
+++
@@ -1,7 +0,0 @@
-import glamorous, { ThemeProvider, withTheme } from 'glamorous'
-import assignAliases from './aliases'
-
-// For React
-export default assignAliases(glamorous)
-
-export { ThemeProvider, withTheme }
