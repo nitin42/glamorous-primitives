8c8359c13c7b648d66d95dc44d4af5e9ffab48ca
Nitin Tulswani <tulswani19@gmail.com>
2017-08-03 22:50:21 +0530

    Delete tiny.js

--- src/tiny.js
+++
@@ -1,9 +0,0 @@
-import createGlamorous from './constructor/create-glamorous';
-
-function splitProps({ theme, innerRef, glam, ...rest }) {
-	return { toForward: rest, styleOverrides: {} };
-}
-
-const glamorous = createGlamorous(splitProps);
-
-export default glamorous;
